5efabdc2208bdebb1f8e3278d49177bdbdfac7bf
RakotoNix <rakotomalalanirinasoa92@gmail.com>
2021-06-24 18:37:43 +0300

    Mise en place administration

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,6 +8,7 @@ use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use App\Entity\Booking;
+use App\Entity\Comment;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -26,7 +27,7 @@ class AppFixtures extends Fixture
         $faker = Factory::create('FR-fr');
 
         $adminRole = new Role();
-        $adminRole->setTitle('ADMIN_ROLE');
+        $adminRole->setTitle('ROLE_ADMIN');
         $manager->persist($adminRole);
 
         $adminUser = new User();
@@ -122,6 +123,17 @@ class AppFixtures extends Fixture
                         ->setComment($comment);
 
                 $manager->persist($booking);
+
+                // Gestion des commentaires
+                if(mt_rand(0, 1))
+                {
+                    $comment = new Comment();
+                    $comment->setContent($faker->paragraph())
+                            ->setRating(mt_rand(1, 5))
+                            ->setAuthor($booker)
+                            ->setAd($ad);
+                    $manager->persist($comment);
+                }
             }
             
             $manager->persist($ad);
